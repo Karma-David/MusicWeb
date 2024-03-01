4c8b9413008d915f5ffe8e0bd0f6c835b7177991
Karma-David <quytre19042003@gmail.com>
2024-03-01 13:43:50 +0700

    test

--- src/java/Model/MusicCart.java
+++ src/java/Model/MusicCart.java
@@ -9,5 +9,55 @@ package Model;
  * @author Hi
  */
 public class MusicCart {
+    private int CartNum, UserID, CartItemID;
+    private double TotalPrice;
+
+    public MusicCart() {
+    }
+
+    public MusicCart(int CartNum, int UserID, int CartItemID, double TotalPrice) {
+        this.CartNum = CartNum;
+        this.UserID = UserID;
+        this.CartItemID = CartItemID;
+        this.TotalPrice = TotalPrice;
+    }
+
+    public int getCartNum() {
+        return CartNum;
+    }
+
+    public void setCartNum(int CartNum) {
+        this.CartNum = CartNum;
+    }
+
+    public int getUserID() {
+        return UserID;
+    }
+
+    public void setUserID(int UserID) {
+        this.UserID = UserID;
+    }
+
+    public int getCartItemID() {
+        return CartItemID;
+    }
+
+    public void setCartItemID(int CartItemID) {
+        this.CartItemID = CartItemID;
+    }
+
+    public double getTotalPrice() {
+        return TotalPrice;
+    }
+
+    public void setTotalPrice(double TotalPrice) {
+        this.TotalPrice = TotalPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "MusicCart{" + "CartNum=" + CartNum + ", UserID=" + UserID + ", CartItemID=" + CartItemID + ", TotalPrice=" + TotalPrice + '}';
+    }
+    
     
 }
